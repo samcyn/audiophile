f93d4d31c1e969f87cedfd0fc24846a037e4beca
samcyn <samsoniyanda@outlook.com>
2023-03-04 01:06:52 +0000

    chore: set up tailwind theming

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,27 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: {
+        100: '#000000',
+        90: '#101010'
+      },
+      white: '#ffffff',
+      grey: {
+        100: '#f1f1f1',
+        90: '#fafafa'
+      },
+      orange: {
+        100: '#d87d4a',
+        90: '#fbaf85'
+      },
+    },
+    // fontFamily: {
+    //   sans: ['Graphik', 'sans-serif'],
+    //   serif: ['Manrope', 'serif'],
+    // },
     extend: {},
   },
   plugins: [],
